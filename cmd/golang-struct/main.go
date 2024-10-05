a0dcd8ddde825564c99e7e6ac977333e25075dd5
teno <teno@peppercon.co.kr>
2024-10-06 02:01:49 +0900

    실습2

--- cmd/golang-struct/main.go
+++ cmd/golang-struct/main.go
@@ -11,14 +11,23 @@ type House struct {
 }
 
 func main() {
-	var house House
-	house.Address = "서울시 강동구..."
-	house.Size = 28
-	house.Price = 9.8
-	house.Type = "아파트"
+	var house1 House	// 기본값
+	var house2 House = House{"서울시 강동구...", 28, 9.80, "아파트"}
+	var house3 House = House{
+		"서울시 강동구...",
+		28,
+		9.80,
+		"아파트",
+	}
+	var house4 House = House{ Size: 28, Type: "아파트" }
+	var house5 House = House{
+		Size: 28,
+		Type: "아파트",
+	}
 
-	fmt.Println("주소:", house.Address)
-	fmt.Printf("크기: %d평\n", house.Size)
-	fmt.Printf("가격: %.2f억 원\n", house.Price)
-	fmt.Println("타입:", house.Type)
+	fmt.Printf("house1 크기: %d평\n", house1.Size)
+	fmt.Printf("house2 크기: %d평\n", house2.Size)
+	fmt.Printf("house3 크기: %d평\n", house3.Size)
+	fmt.Printf("house4 크기: %d평\n", house4.Size)
+	fmt.Printf("house5 크기: %d평\n", house5.Size)
 }
